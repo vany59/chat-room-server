f8ef4004621ff6245f3fded587d780cc93feb16c
y.levan <levanyy2@gmail.com>
2024-11-28 00:01:42 +0700

    feat: add compression middleware to optimize response size

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
+  app.use(compression());
 
   const configService = app.get(ConfigService);
 
