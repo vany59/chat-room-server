064fa609249f9174216ed59308b62cb6c908a26c
y.levan <levanyy2@gmail.com>
2024-11-29 04:13:23 +0700

    feat(user): update entity

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,10 +1,31 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Room } from '../rooms/room.entity';
+import { RoomParticipant } from '../rooms/room-participant.entity';
 
 @Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column({ unique: true })
   userName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Room, (room) => room.creator)
+  createdRooms: Room[];
+
+  @OneToMany(() => RoomParticipant, (participant) => participant.user)
+  participatingRooms: RoomParticipant[];
 }
