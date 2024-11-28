b67431fc03397aec77d4740338aaa2e3a4873737
y.levan <levanyy2@gmail.com>
2024-11-29 04:14:12 +0700

    feat(swagger): add bearer auth

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,17 @@ async function bootstrap() {
       .setTitle('Chat room API')
       .setDescription('The Chat room API documentation')
       .setVersion('1.0')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        },
+        'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      )
       .build();
 
     const documentFactory = () =>
